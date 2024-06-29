5a1d59764c94ea494563fa6ac5082b0c95be19e5
apguilar <andreapaguilart@gmail.com>
2024-06-29 03:58:40 -0400

    Se colocaron comentarios explicativos y detallados en el código para el desplazamiento en la pista con obstáculos.

--- main.py
+++ main.py
@@ -6,33 +6,46 @@ def on_button_pressed_b():
     pass
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+TIEMPO_CORTO = 0
+TIEMPO = 0
 basic.show_icon(IconNames.HAPPY)
+# Se fija el valor de la variable "GRADOS" a 90. Para que al iniciar el programa, el robot verifique el ángulo en que se encuentra el servo-motor, y lo coloque a 90°.
 GRADOS = 90
 neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, GRADOS)
-# Ult modificación
-# 
-# Pausa 100 + lo de abajo
 
 def on_forever():
-    global GRADOS
+    global GRADOS, TIEMPO, TIEMPO_CORTO
     GRADOS = 70
-    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J2,
-        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) > 0 and PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J2,
-        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) < 160:
+    TIEMPO = 700
+    TIEMPO_CORTO = 70
+    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J4,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) > 0 and PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J4,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) < 90:
         neZha.set_motor_speed(neZha.MotorList.M1, 25)
         neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
-    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J2,
-        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) == 0:
+    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J4,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) < 0:
         neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, 60)
         neZha.set_motor_speed(neZha.MotorList.M1, 25)
-    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J1,
-        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) > 0 and PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J1,
-        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) < 13:
+    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J3,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) > 0 and PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J3,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) < 20:
         neZha.set_motor_speed(neZha.MotorList.M1, 25)
         GRADOS += -15
         neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, GRADOS)
-        basic.pause(100)
-        GRADOS += 8
+        TIEMPO_CORTO += 10
+        basic.pause(TIEMPO_CORTO)
+        GRADOS += 9
         neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, GRADOS)
+    while PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J1,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) > 0 and PlanetX_Basic.ultrasound_sensor(PlanetX_Basic.DigitalRJPin.J1,
+        PlanetX_Basic.Distance_Unit_List.DISTANCE_UNIT_CM) < 25:
+        neZha.set_motor_speed(neZha.MotorList.M1, -25)
+        neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, 110)
+        basic.pause(TIEMPO)
     neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
 basic.forever(on_forever)
+
+def on_forever2():
+    pass
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,33 +1,93 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+// Segmento de código para el reconocimiento de las señales de tráfico. Inicia al presionarse el botón A de la tarjeta micro:bit. 
+// 
+// Al ejecutarse, inicializa la cámara y la configura, así como determina el valor 0 para la variable "Número". Además, fija el ángulo del servo-motor a 90° y enciende el motor DC con una potencia del 25%.
+input.onButtonPressed(Button.A, function () {
+    // Inicializa la cámara para su uso.
+    PlanetX_AILens.initModule()
+    // Determina la función de reconocimiento de la cámara para: Detección de color.
+    PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Color)
+    Número = 0
+    neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
+    neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+    // Bucle que confirma la igualdad entre la dos variables para ejecutar el código.
+    while (Número == Número) {
+        // Realiza una pausa de 0.1 segundos antes de cada repetición.
+        basic.pause(100)
+        // Analiza la imagen detectada por la cámara.
+        PlanetX_AILens.cameraImage()
+        // Indica que cuando la cámara detecte el color verde, se fijará el ángulo del servo-motor a 105° y la potencia del motor DC a 25%. Luego de 1 segundo, fijará el ángulo del servo-motor a 70°. Y luego de 1 segundo, fijará el ángulo a 90°; para posteriormente realizar una pausa de 0,2 segundos.
+        // 
+        // 
+        // Esto permitirá al vehículo esquivar la señal de tráfico verde por el lado izquierdo.
+        if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.green)) {
+            neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 105)
+            neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+            basic.pause(1000)
+            neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 70)
+            basic.pause(1000)
+            neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
+            basic.pause(200)
+        }
+        // Indica que cuando la cámara detecte el color rojo, se fijará el ángulo del servo-motor a 70° y la potencia del motor DC a 25%. Luego de 1 segundo, fijará el ángulo del servo-motor a 105°. Y luego de 1 segundo, fijará el ángulo a 90°; para posteriormente realizar una pausa de 0,2 segundos.
+        // 
+        // Esto permitirá al vehículo esquivar la señal de tráfico roja por el lado derecho.
+        if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.red)) {
+            neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 70)
+            neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+            basic.pause(1000)
+            neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 105)
+            basic.pause(1000)
+            neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
+            basic.pause(200)
+        }
+    }
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-})
+// Acciones que se ejecutan al encender el vehículo. Comienza mostrando una imagen en la tarjeta micro:bit, que permite identificar el programa.
+let TIEMPO_CORTO = 0
+let TIEMPO = 0
+let Número = 0
 basic.showIcon(IconNames.Happy)
+// Se fija el valor de la variable "GRADOS" a 90. Para que al iniciar el programa, el robot verifique el ángulo en que se encuentra el servo-motor, y lo coloque a 90°.
 let GRADOS = 90
+// Coloca el servo-motor a los grados que indica la variable.
 neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, GRADOS)
-//  Ult modificación
-//  
-//  Pausa 100 + lo de abajo
-basic.forever(function on_forever() {
-    
+// Acciones que se repiten en bucle desde que se enciende el vehículo. Inicialmente se declaran las variables "GRADOS", "TIEMPO", y "TIEMPO_CORTO"
+basic.forever(function () {
     GRADOS = 70
-    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 0 && PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) < 160) {
+    TIEMPO = 700
+    TIEMPO_CORTO = 80
+    // Indica que mientras el sensor de ultrasonido conectado al puerto J4 detecte una distancia mayor de 0cm y menor de 90cm, encienda el motor DC a una potencia de 25% y fije el ángulo del servo-motor a 90°.
+    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J4, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 0 && PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J4, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) < 90) {
         neZha.setMotorSpeed(neZha.MotorList.M1, 25)
         neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
     }
-    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) == 0) {
+    // Indica que mientras el sensor de ultrasonido conectado al puerto J4 detecte una distancia igual a 0cm, encienda el motor DC a una potencia de 25% y fije el ángulo del servo-motor a 60°. Esto le permite realizar los giros en la pista.
+    // 
+    // El vehículo detecta la distancia de la barrera interna de la pista y se mantiene conduciendo de forma recta. Una vez que ya no la detecta, comienza a girar hasta detectarla nuevamente.
+    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J4, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) == 0) {
         neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 60)
         neZha.setMotorSpeed(neZha.MotorList.M1, 25)
     }
-    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 0 && PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) < 13) {
+    // Indica que mientras el sensor de ultrasonido conectado al puerto J3 detecte una distancia mayor de 0cm y menor de 20cm, encienda el motor DC a una potencia de 25% y reste 15° al ángulo del servo-motor. Luego de 0,08 segundos, añade 9° al ángulo del vehículo.
+    // 
+    // Esta función permite que cuando el vehículo detecte una barrera a menos de 20cm por su lado izquierdo, no choque con ella.
+    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J3, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 0 && PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J3, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) < 20) {
         neZha.setMotorSpeed(neZha.MotorList.M1, 25)
         GRADOS += -15
         neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, GRADOS)
-        basic.pause(100)
-        GRADOS += 8
+        TIEMPO_CORTO += 10
+        basic.pause(TIEMPO_CORTO)
+        GRADOS += 9
         neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, GRADOS)
     }
+    // Indica que mientras el sensor de ultrasonido conectado al puerto J1 detecte una distancia mayor de 0cm y menor de 25cm, que encienda el motor DC a una potencia de -25% para que el vehículo retroceda. Y fije el ángulo del servo-motor a 110°, por un tiempo de 0,7 segundos.
+    // 
+    // De esta manera, si el vehículo se dirige de frente hacia una barrera, es capaz de retroceder y rectificar su trayectoria.
+    while (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 0 && PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) < 25) {
+        neZha.setMotorSpeed(neZha.MotorList.M1, -25)
+        neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 110)
+        basic.pause(TIEMPO)
+    }
+    // Indica que el ángulo del servo-motor sea fijado a 90°.
     neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 90)
 })
